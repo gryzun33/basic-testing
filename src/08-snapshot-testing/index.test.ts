5745e7343092fda1963efde94dedfa9495044128
gryzun33 <yakushevao777@gmail.com>
2024-10-21 17:26:19 +0300

    feat: add snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,31 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const elements = [1, 5, 6];
+    const expected = {
+      value: 1,
+      next: {
+        value: 5,
+        next: {
+          value: 6,
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    };
+    const linkedList = generateLinkedList(elements);
+
+    expect(linkedList).toStrictEqual(expected);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const elements = [1, 5, 7];
+    const linkedList = generateLinkedList(elements);
+    expect(linkedList).toMatchSnapshot();
   });
 });
